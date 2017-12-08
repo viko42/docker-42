fbcaf6d1c721ceaddd8970e158237a965abe17fe
Viko@raspberry <victor@stationf.co>
2017-12-08 13:51:28 +0100

    viko

--- 01_dockerfiles/ex01/install.sh
+++ 01_dockerfiles/ex01/install.sh
@@ -1,3 +1,1 @@
-docker rm ts3container
-docker build -t ts3 . && docker run --name ts3container -t -i -d -p 9987:9987/udp -p 30033:30033 -p 10011:10011 ts3
-docker logs ts3container
+docker build -t ex01Image . && docker run --name ex01 -it -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -d ex01Image && docker logs ex01
